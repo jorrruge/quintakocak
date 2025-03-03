ffda3dba2a8c947cd2843340f45d037e85415307
jorrruge <110229736+jorrruge@users.noreply.github.com>
2025-03-03 18:19:03 +0700

    Create quintagemes.py

---
+++ quintagemes.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+def main():
+    st.title("💖 Untuk Quinta Elvira Rosiana Sipahutar 💖")
+    
+    st.write("I Love You, Quinta Elvira Rosiana Sipahutar")
+    st.write("Terus berjuang bareng aku ya dan bahagia bareng aku")
+    st.write("Ayo kita lewatin ini bareng-bareng")
+    
+    st.markdown("""
+    ```
+      *****     *****
+    **     ** **     **
+   **       ***       **
+    **               **
+      **           **
+        **       **
+          **   **
+            **
+    ```
+    """)
+    
+if __name__ == "__main__":
+    main()
